f41cd2ebae678c9130baf6f3977317532e7ff3ea
rrodr630 <robertrodriguez0129@gmail.com>
2020-10-13 10:30:52 -0400

    Added helper methods to validate entry fields. Added scrolldown menus to patient. Event handler function for start recording button as well as validate type conversions

--- Data-recording-UI/Controller.py
+++ Data-recording-UI/Controller.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from Design import *
 from tkinter import ttk, Entry
+import HelperMethods as hm
 
 
 class Controller(tk.Tk):
@@ -86,6 +87,16 @@ class HomePage(tk.Frame):
         # endregion
 
 class RecordingPage(tk.Frame):
+
+    # values for all labels
+    ageValue = 0
+    weightValue = 0
+    heightValue = 0
+    nameValue = ""
+    skinColorValue = ""
+    raceOptionSelected = ""
+    ethnicityOptionSelected = ""
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
@@ -108,22 +119,32 @@ class RecordingPage(tk.Frame):
         weightEntry = Entry(self)
         weightEntry.pack()
 
+        heightLabel = ttk.Label(self, text="Height", font=SMALL_FONT)
+        heightLabel.pack()
+        heightEntry = Entry(self)
+        heightEntry.pack()
+
         ethnicityLabel = ttk.Label(self, text="Ethnicity", font=SMALL_FONT)
         ethnicityLabel.pack()
-        ethnicityEntry = Entry(self)
-        ethnicityEntry.pack()
+        self.ethnicityOptionSelected = tk.StringVar()
+        self.ethnicityOptionSelected.set(hm.Ethnicity[0])   # Initial value
+        ethnicityOptions = tk.OptionMenu(self, self.ethnicityOptionSelected, *hm.Ethnicity)
+        ethnicityOptions.pack()
+
 
         raceLabel = ttk.Label(self, text="Race", font=SMALL_FONT)
         raceLabel.pack()
-        raceEntry = Entry(self)
-        raceEntry.pack()
+        self.raceOptionSelected = tk.StringVar()
+        self.raceOptionSelected.set(hm.Race[0])             # Initial value
+        raceOptions = tk.OptionMenu(self, self.raceOptionSelected, *hm.Race)
+        raceOptions.pack()
 
         skinColorLabel = ttk.Label(self, text="Skin Color", font=SMALL_FONT)
         skinColorLabel.pack()
         skinColorEntry = Entry(self)
         skinColorEntry.pack()
 
-        button1 = ttk.Button(self, text="Start recording")
+        button1 = ttk.Button(self, text="Start recording", command=lambda: start_process())
         button1.pack(pady=10, padx=10)
 
         button2 = ttk.Button(self, text="Pause recording")
@@ -137,9 +158,73 @@ class RecordingPage(tk.Frame):
 
         button5 = ttk.Button(self, text="Back to Home", command=lambda: controller.show_frame(HomePage))
         button5.pack(pady=10, padx=10)
+        # endregion
 
-        #endregion
+        # verify all fields first, then connect to db and send all info
+        def start_process():
+            # getValues stores all values from fields into variables and returns any errors found while trying to
+            # convert each field into its respective type
+            error = getValues()
 
-        
+            if len(error) == 0:     # no errors
+                # set up connection and send values
+                print(self.ageValue)
+                print(self.heightValue)
+                print(self.weightValue)
+                print(self.ethnicityOptionSelected.get())
+                print(self.raceOptionSelected.get())
+                print(self.skinColorValue)
+
+                return
+            else:
+                # display pop-up dialog box with error message
+                print(error)
+                return
+
+        def getValues():
+
+            ErrorMessage = ""
+
+            try:
+                self.ageValue = int(ageEntry.get())
+            except ValueError:
+                ErrorMessage += "Value entered for age is not a number.\n"
+
+            try:
+                self.heightValue = float(heightEntry.get())
+            except ValueError:
+                ErrorMessage += "Value entered for height is not a number.\n"
+
+            try:
+                self.weightValue = int(weightEntry.get())
+            except ValueError:
+                ErrorMessage += "Value entered for weight is not a number.\n"
+
+            self.skinColorValue = skinColorEntry.get()
+            if hm.isEmpty(self.skinColorValue):
+                ErrorMessage += "No entry for skin color.\n"
+
+            self.nameValue = nameEntry.get()
+            if hm.isEmpty(self.nameValue):
+                ErrorMessage += "No entry for name.\n"
+
+            if hm.isScrollDownMenuWrong(self.ethnicityOptionSelected.get()):
+                ErrorMessage += "Please select an option for ethnicity.\n"
+
+            if hm.isScrollDownMenuWrong(self.raceOptionSelected.get()):
+                ErrorMessage += "Please select an option for race.\n"
+
+            if not hm.isAgeValid(self.ageValue):
+                ErrorMessage += "Value of age is invalid.\n"
+
+            if not hm.isWeightValid(self.weightValue):
+                ErrorMessage += "Value of weight is invalid.\n"
+
+            if not hm.isHeightValid(self.heightValue):
+                ErrorMessage += "Value of height is invalid.\n"
+
+            return ErrorMessage
+
+
 app = Controller()
 app.mainloop()
